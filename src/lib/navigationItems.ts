923c827beb129b7a933cf7268c90277b174fd449
seyza <pedrotychang@gmail.com>
2024-12-28 12:08:16 -0800

    remove home button on the corner

--- src/lib/navigationItems.ts
+++ src/lib/navigationItems.ts
@@ -1,23 +1,5 @@
-// export const navigationItems = [
-//   {
-//     name: "Home",
-//     href: "/",
-//   },
-//   {
-//     name: "Resources",
-//     href: "/resources",
-//   },
-//   {
-//     name: "About",
-//     href: "/about",
-//   },
-// ];
-
-// In lib/navigationItems.ts
 export const navigationItems = [
-  { name: "Home", href: "/" },
   { name: "About", href: "/about" },
   { name: "Resources", href: "/resources" },
   { name: "Contact", href: "/contact" },
-  // ... any other navigation items
 ];
